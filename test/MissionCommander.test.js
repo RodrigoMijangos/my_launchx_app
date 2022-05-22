ddb6bbca93f2c9af22e0f5570e10a4ea0d8b3f60
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2022-05-21 20:59:31 -0500

    Test suit for mission commander class went ok

--- test/MissionCommander.test.js
+++ test/MissionCommander.test.js
@@ -1,7 +1,18 @@
+/* 
+Older test suit
 describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1', () => {
       const result = 1 + 2 
       expect(result).toBe(3);
     });
   })
-  
+   */
+
+  const MC = require('./../app/MissionCommander.js');
+
+  describe('Suit test from MissionCommander class', () => {
+    test('1. Create a mission commander object', () => {
+      const missionCommander = new MC("Woopa");
+      expect(missionCommander.name).toBe("Woopa");
+    });
+  });
